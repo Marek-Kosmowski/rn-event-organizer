31af826fa35c1a5cfa734f9db722688af875595a
Marek Kosmowski <kosmowski.marek@gmail.com>
2025-05-17 22:34:21 +0200

    Add fonts configuration to app.json, add SpashScreen to App.js

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+/// FONTS
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [loaded, error] = useFonts({
+    Anton: require('./assets/fonts/Anton.ttf'),
+    Pacifico: require('./assets/fonts/Pacifico.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontFamily: 'Anton' }}>
+        Open up App.js to start working on your app!!!
+      </Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
