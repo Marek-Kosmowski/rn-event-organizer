4775c73dac9529f0ef0d8d3bce459cb22b9eeb66
Marek Kosmowski <kosmowski.marek@gmail.com>
2025-05-25 21:46:30 +0200

    Create constants file to keep colors, in Auth.js create states, styles, text inputs

--- screens/auth.js
+++ screens/auth.js
@@ -1,5 +1,72 @@
-import { Text } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, TextInput } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { AppStyle } from '../utils/constants';
+import { useState } from 'react';
 
 export default function AuthScreen() {
-  return <Text>LOGIN SCREEN</Text>;
+  const [type, setType] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <LinearGradient
+      style={styles.constainer}
+      colors={[AppStyle.safflower, AppStyle.hibiscus]}
+    >
+      <SafeAreaView style={styles.form}>
+        <Text style={styles.title}>{type ? 'Sign up' : 'Sign in'}</Text>
+        <TextInput
+          style={styles.input}
+          placeholderTextColor='#B0B0B0'
+          placeholder='Enter email'
+          autoCapitalize='none'
+          keyboardType='email-address'
+          textContentType='emailAddress'
+          autoFocus={true}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor='#B0B0B0'
+          placeholder='Enter Password'
+          autoCapitalize='none'
+          autoCorrect={false}
+          secureTextEntry={true}
+          textContentType='password'
+          value={password}
+          onChange={(text) => setPassword(text)}
+        />
+      </SafeAreaView>
+    </LinearGradient>
+  );
 }
+
+const styles = StyleSheet.create({
+  constainer: {
+    flex: 1,
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 40,
+  },
+  title: {
+    fontFamily: 'Anton',
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#fffffe',
+    paddingBottom: 20,
+  },
+  input: {
+    backgroundColor: '#1E1E1E',
+    borderWidth: 1,
+    borderColor: AppStyle.borderColor,
+    height: 50,
+    marginBottom: 20,
+    fontSize: 20,
+    borderRadius: 8,
+    padding: 10,
+    color: '#FFFFFF',
+  },
+});
---
+++ utils/constants.js
@@ -0,0 +1,9 @@
+export const AppStyle = {
+  hibiscus: '#ba2f70',
+  trafficRed: '#FD1D1D',
+  safflower: '#fcb045',
+  background: '#0f0e17',
+  paragraph: '#a7a9be',
+  borderColor: '#2C2C2C',
+  error: '#FF3D00',
+};
